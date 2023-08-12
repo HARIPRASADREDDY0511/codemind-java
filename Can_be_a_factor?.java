437d81cd2f830e2b8293900b9d9bed76c1de5d79
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-08-13 01:16:19 +0530

    import java.util.*; public class Main{     public static void main(String args[]){         Scanner s = new Scanner(System.in);         int n = s.nextInt();         for (int i = 0; i <n; i++) {             int a = s.nextInt();             int b = s.nextInt();             if(a==b)             {                 System.out.println("YES");             }             else if((b/a)>1)             {                 System.out.println("YES");             }             else{                 System.out.println("NO");             }
            }
        }
    }

---
+++ Can_be_a_factor?.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();  
+        for (int i = 0; i <n; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            if(a==b)
+            {
+                System.out.println("YES");
+            }
+            else if((b/a)>1)
+            {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            
+        }
+    }
+}
