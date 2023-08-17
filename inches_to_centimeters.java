26f49d02490d63db85346aefb4c7fdfe2a7091f7
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-08-17 11:04:48 +0530

    import java.util.*; public class hr{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int h=sc.nextInt();         double height;         height=2.54*h;         System.out.printf("%.2f",height);     } }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hr{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int h=sc.nextInt();
+        double height;
+        height=2.54*h;
+        System.out.printf("%.2f",height);
+    }
+}
