2b329f267791be6be1e5767903a434d0c5b3ce7d
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-08-17 11:23:58 +0530

    import java.util.*; public class hr1{     public static void main(String args[])     {         int a,b,c;         Scanner h=new Scanner(System.in);         a=h.nextInt();         b=h.nextInt();         c=h.nextInt();         System.out.printf("%d",(c-b)/a);     } }

---
+++ Can_cross_the_bridge.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class hr1{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner h=new Scanner(System.in);
+        a=h.nextInt();
+        b=h.nextInt();
+        c=h.nextInt();
+        System.out.printf("%d",(c-b)/a);
+    }
+}
