14b1f511d2854862d3fcdde6d83ccb1c320c09b5
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-08-18 11:52:05 +0530

    import java.util.*; public class hr{     public static void main(String args[]){         int n,remainder,reversed=0,original;         Scanner h=new Scanner(System.in);         n=h.nextInt();         original=n;         while(n!=0){             remainder=n%10;             reversed=reversed * 10 + remainder;             n/=10;         }         if(reversed==original)             System.out.println("Palindrome");         else             System.out.println("Not Palindrome");     } }

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class hr{
+    public static void main(String args[]){
+        int n,remainder,reversed=0,original;
+        Scanner h=new Scanner(System.in);
+        n=h.nextInt();
+        original=n;
+        while(n!=0){
+            remainder=n%10;
+            reversed=reversed * 10 + remainder;
+            n/=10;
+        }
+        if(reversed==original)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
