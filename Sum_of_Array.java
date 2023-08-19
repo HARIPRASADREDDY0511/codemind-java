7b1463affa1ac5e85cf342cea545acf6c260b089
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-08-19 11:52:24 +0530

    import java.util.Scanner; public class solution {     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         // Array declaration         int[] intArray=new int[n];         for(int i=0;i<n;i++){             intArray[i]=sc.nextInt();         }         int sum=0;         for(int eachElement: intArray) {             sum +=eachElement;         }         System.out.println(sum);
        }
    }

---
+++ Sum_of_Array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class solution {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        // Array declaration
+        int[] intArray=new int[n];
+        for(int i=0;i<n;i++){
+            intArray[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int eachElement: intArray) {
+            sum +=eachElement;
+        }
+        System.out.println(sum);
+        
+    }
+}
