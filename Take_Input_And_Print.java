bd81cf62fa4541284982b81fbb1f94a3faea7305
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-09-01 09:56:01 +0530

    import java.util.Scanner; public class Hari{     public static void main(String args[]){         Scanner hr=new Scanner(System.in);         String a=hr.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Hari{
+    public static void main(String args[]){
+        Scanner hr=new Scanner(System.in);
+        String a=hr.nextLine();
+        System.out.print(a);
+    }
+}
