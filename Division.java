0d4ad6340a5245eb0822a421d87b2e9e2c0c5ea2
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-09-01 09:52:34 +0530

    import java.util.Scanner; public class Hari{     public static void main(String args[]){         Scanner hr=new Scanner(System.in);         int a=hr.nextInt();         int b=hr.nextInt();         System.out.print(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Hari{
+    public static void main(String args[]){
+        Scanner hr=new Scanner(System.in);
+        int a=hr.nextInt();
+        int b=hr.nextInt();
+        System.out.print(a/b);
+    }
+}
